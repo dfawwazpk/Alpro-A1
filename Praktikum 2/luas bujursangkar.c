59561907b527d4243bbea39c90508aa00c78e8aa
Dimas <90162065+dfawwazpk@users.noreply.github.com>
2022-03-01 11:21:44 +0700

    Add files via upload

---
+++ Praktikum 2/luas bujursangkar.c
@@ -0,0 +1,19 @@
+/* Nama = Dimas Fawwaz Prabowo Kusumaji */
+/* NIM = 24060121140035 */
+/* Tugas Praktikum 2 Alpro A1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int S;
+
+printf("Masukan Sisi (S) = ");
+scanf("%d",&S);
+
+printf("Luas bujursangkar = %d x %d = %d",S,S,S*S);
+return 0;
+
+}
